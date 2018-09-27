1cae6ab1a7d94055accf79a911af64bc9b79b29d
Vlad <volkov8991@gmail.com>
2018-09-27 14:20:12 +0300

    f

--- app/code/local/CP/WShop/controller/IndexController.php
+++ app/code/local/CP/WShop/controller/IndexController.php
@@ -6,7 +6,7 @@
  * Date: 27.09.2018
  * Time: 13:22
  */
-class WShop_IndexController extends Mage_Core_Controller_Front_Action
+class CP_WShop_IndexController extends Mage_Core_Controller_Front_Action
 {
     public function indexAction()
     {
